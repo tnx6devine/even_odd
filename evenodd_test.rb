663750d0dcacfc621022ac6cca1a2aaa78aeca0d
tnx6devine <tnx6devine@gmail.com>
2017-11-19 10:12:57 -0500

    Tested for 3 is odd, fail

--- evenodd_test.rb
+++ evenodd_test.rb
@@ -18,4 +18,12 @@ class TestEvenOdd < Minitest::Test
 		assert_equal("odd", numberEval(1))
 	end
 
+	def test_assert_that_2_is_even
+		assert_equal("even", numberEval(2))
+	end
+
+	def test_assert_that_3_is_odd
+		assert_equal("odd", numberEval(3))
+	end
+
 end
